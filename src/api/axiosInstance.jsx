f941c5c75e25047067a5712fef085a043a75e4fe
joynul24 <joynula919@gmail.com>
2026-06-04 14:10:30 +0600

    remove api text in axiosInstance.jsx file

--- src/api/axiosInstance.jsx
+++ src/api/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://portfolio-backend-eight-sepia.vercel.app/api",
+  baseURL: "https://portfolio-backend-eight-sepia.vercel.app",
 });
 
 export default axiosInstance;
