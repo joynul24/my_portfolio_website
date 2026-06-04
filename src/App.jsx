2781b89e5a3c6f3a70ce481709ee052226d1c876
joynul24 <joynula919@gmail.com>
2026-06-04 13:29:55 +0600

    fix react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,3 @@
-
-// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-
-// const queryClient = new QueryClient();
-
-// export default function App() {
-//   return (
-//     <QueryClientProvider client={queryClient}>
-//       <Router>
-//         <div className="font-sans min-h-screen text-white selection:bg-cyan-500 selection:text-white relative z-0">
-//           <AnimatedBackground />
-//           <Routes>
-//             <Route path="/" element={<Home />} />
-//             <Route path="/admin" element={<AdminDashboard />} />
-//           </Routes>
-//           <BackToTop />
-//           <Toaster position="top-right"
-//             toastOptions={{
-//               className: 'bg-slate-800 text-white',
-//               style: { background: '#1e293b', color: '#fff' }
-//             }}
-//           />
-//         </div>
-//       </Router>
-//     </QueryClientProvider>
-//   );
-// }
-
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
