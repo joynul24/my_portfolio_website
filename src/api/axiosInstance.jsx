bec61f8f5391973b00233c4a0dd39e41e6515a5f
joynul24 <joynula919@gmail.com>
2026-06-04 19:38:30 +0600

    timeout: 15000 added axiosIntance file

--- src/api/axiosInstance.jsx
+++ src/api/axiosInstance.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "https://portfolio-backend-eight-sepia.vercel.app",
+  timeout: 15000
 });
 
 export default axiosInstance;
