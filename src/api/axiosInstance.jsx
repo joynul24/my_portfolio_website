c0ba8b651d61cf7f4ef6865f1cd6b4a8972eb067
joynul24 <joynula919@gmail.com>
2026-06-05 22:50:45 +0600

    change timeout in axiosInstence file

--- src/api/axiosInstance.jsx
+++ src/api/axiosInstance.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "https://portfolio-backend-eight-sepia.vercel.app",
-  timeout: 2000
+  timeout: 10000
 });
 
 export default axiosInstance;
