d03f073cdb14ee23663e1bcf9b2c3fa5675a56d5
joynul24 <joynula919@gmail.com>
2026-06-12 22:15:16 +0600

    Add SEO meta tags

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,18 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-
-
-
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './index.css';
 import App from './App.jsx';
+import { HelmetProvider } from 'react-helmet-async';
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+     <HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </HelmetProvider>
   </StrictMode>
 );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,6 +9,7 @@ import Loader from '../components/Loader';
 import MySkills from '../sections/MySkills';
 import Services from '../sections/Services';
 import Experience from '../sections/Experience';
+import { Helmet } from 'react-helmet-async';
 
 const Home = () => {
   const [loading, setLoading] = useState(true);
@@ -24,15 +25,34 @@ const Home = () => {
 
   return (
     <div className="relative">
+
+      <Helmet>
+        <title>
+          DevJoynul26 | Joynul Abedin | MERN Stack Developer
+        </title>
+
+        <meta
+          name="description"
+          content="Joynul Abedin (DevJoynul26) is a MERN Stack Developer from Bangladesh. Explore my portfolio, projects, skills and contact information."
+        />
+
+        <meta
+          name="keywords"
+          content="DevJoynul26, Joynul Abedin, MERN Stack Developer, React Developer, Full Stack Developer, Bangladesh"
+        />
+
+        <meta name="author" content="Joynul Abedin" />
+      </Helmet>
+
       <Navbar />
       <main>
         <Hero />
         <About />
         <Services></Services>
-        <MySkills></MySkills> 
+        <MySkills></MySkills>
         <Experience></Experience>
         <Projects />
-        <Contact /> 
+        <Contact />
       </main>
       <Footer />
     </div>
